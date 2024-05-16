74f33cb9c1d2093fe7d92f984218071cdb31c167
Alexandru Maftei <mafterr11@gmail.com>
2024-05-16 14:23:10 +0300

    .

--- src/components/sections/CTA.tsx
+++ src/components/sections/CTA.tsx
@@ -1,16 +1,62 @@
 import React from "react";
 import Header from "../common/Header";
-import MainButton from "../common/MainButton";
+import {
+  Calendar,
+  GraduationCap,
+  MailIcon,
+  PhoneCall,
+  User2,
+} from "lucide-react";
+
+const infoData = [
+  {
+    icon: <User2 size={20} />,
+    text: "Diana Maftei",
+  },
+  {
+    icon: <PhoneCall size={20} />,
+    text: "+40.724.411.148",
+  },
+  {
+    icon: <MailIcon size={20} />,
+    text: "mafteidiana39@gmail.com",
+  },
+  {
+    icon: <Calendar size={20} />,
+    text: "03 Sep, 1997",
+  },
+  {
+    icon: <GraduationCap size={20} />,
+    text: "Universitatea Nationala de Arte Bucuresti",
+  },
+];
 
 function NewsletterSection() {
   return (
     <section id="contact" className="my-20">
-      <div className="flex flex-col items-center justify-center gap-y-6">
-        <Header
-          title="Hai să proiectăm împreună"
-          description="Nu ezitați să mă contactați pentru orice întrebări sau colaborări"
-        />
-        <MainButton text="Contact Me" classes="w-[222px]" />
+      <div className="flex flex-col xl:flex-row items-start md:items-center justify-evenly gap-y-12">
+        <div className="max-w-xl text-center xl:text-left">
+          <p className="mb-10  text-2xl font-semibold leading-snug md:text-[55px]">
+            Hai să proiectăm împreună
+          </p>
+          <p className="text-[21px]">
+            Nu ezitați să mă contactați pentru orice întrebări sau colaborări
+          </p>
+        </div>
+
+        <div className="mb-12 grid gap-x-4 gap-y-8 xl:grid-cols-1">
+          {infoData.map((item, index) => {
+            return (
+              <div
+                className="mx-auto flex items-center gap-x-4 xl:mx-0"
+                key={index}
+              >
+                <div className="text-accent">{item.icon}</div>
+                <p className="text-center xl:text-left">{item.text}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
