f005e4213a8f542c0fdb91e5797769f61e9b52ec
mafterr11 <mafterr11@gmail.com>
2025-02-11 22:34:30 +0200

    .

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -10,7 +10,7 @@ interface IProps {
 function ProjectCard({ imageUrl, title, description }: IProps) {
   return (
     <div className="rounded-[14px] px-[5px] transition-all duration-500 ease-in-out hover:-translate-y-2">
-      <div className="bg-[#FFEBDB] px-[27px] py-4 h-[500px] w-[420px]">
+      <div className="bg-[#FFEBDB] px-[27px] py-4 xl:h-[500px] xl:w-[420px]">
         <Image width={800} height={800} src={imageUrl} alt="service icon" className="w-full h-full object-contain" />
       </div>
       <p className="my-4 mt-8 text-[19px] text-primary">{title}</p>
